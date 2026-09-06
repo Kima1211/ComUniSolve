c37015c9cd2a531fee91217f2c86e78f318f5187
Rojan <kuura1212@gmail.com>
2026-09-06 22:43:40 +0800

    Fixing requirements modification

--- Models/create_tables.py
+++ Models/create_tables.py
@@ -1,5 +1,5 @@
 from database import engine, Base
-import user, problem, solution, comment
+import user, problem, solution, comment,rating
 
 Base.metadata.create_all(bind=engine)
 print("Tables created successfully")
